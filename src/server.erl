c5bafb541e54f6b247887187075923808dac1178
Mirko Kiefer <mail@mirkokiefer.com>
2011-11-03 17:53:00 +0200

    update http interface

--- src/server.erl
+++ src/server.erl
@@ -19,28 +19,34 @@ stop() ->
 
 loop(Req) ->
   Path = Req:get(path),
-  respond(string:tokens(Path, "/"), lists:sort(Req:parse_qs()), Req).
-
-respond(["type", TypeID], _, Req) ->
-  Type = store_interface:read_type(utils:encode(TypeID)),
-  Req:ok({"text/plain;charset=utf-8", utils:json(Type)});
+  Tokens = [utils:encode(Token) || Token <- string:tokens(Path, "/")],
+  respond(Tokens, lists:sort(Req:parse_qs()), Req).
 
 respond([TypeID], _, Req) ->
-  ItemURIs = store:read_items_of_type(utils:encode(TypeID)),
+  ItemURIs = store:read_items_of_type(TypeID),
   Req:ok({"text/plain;charset=utf-8", utils:json(ItemURIs)});
 
-respond([TypeID, "_full"], _, Req) ->
-  io:format("~p~n", [TypeID]),
-  ItemURIs = store:read_items_of_type(utils:encode(TypeID)),
-  io:format("~p~n", [ItemURIs]),
-  Items = [store_interface:read_item(ItemURI, utils:encode(TypeID)) || ItemURI <- ItemURIs],
+respond([<<"type">>, <<"_full">>], _, Req) ->
+  TypeURIs = store:read_items_of_type(?TYPE),
+  Types = [store_interface:read_type(TypeURI) || TypeURI <- TypeURIs],
+  Req:ok({"text/plain;charset=utf-8", utils:json(Types)});
+
+respond([TypeID, <<"_full">>], _, Req) ->
+  ItemURIs = store:read_items_of_type(TypeID),
+  Items = [store_interface:read_item(ItemURI, TypeID) || ItemURI <- ItemURIs],
   Req:ok({"text/plain;charset=utf-8", utils:json(Items)});
 
-respond(["property", URI], _, Req) ->
-  Property = store_interface:read_property(utils:encode(URI)),
+respond([<<"type">>, TypeID], _, Req) ->
+  Type = store_interface:read_type(TypeID),
+  Req:ok({"text/plain;charset=utf-8", utils:json(Type)});
+
+respond([<<"property">>, URI], _, Req) ->
+  Property = store_interface:read_property(URI),
   Req:ok({"text/plain;charset=utf-8", utils:json(Property)});
 
 respond([TypeID, ItemID], _, Req) ->
-  Item = store_interface:read_item(utils:encode(ItemID), utils:encode(TypeID)),
+  Item = store_interface:read_item(ItemID, TypeID),
   io:format("~p~n", [Item]),
   Req:ok({"text/plain;charset=utf-8", utils:json(Item)}).
+
+log(Message) -> io:format("~p~n", [Message]).
