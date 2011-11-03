a50b2637a41cf5d0692d294c7d7e211018133f7a
Mirko Kiefer <mail@mirkokiefer.com>
2011-11-03 20:08:29 +0200

    refactor server

--- src/server.erl
+++ src/server.erl
@@ -24,29 +24,30 @@ loop(Req) ->
 
 respond([TypeID], _, Req) ->
   ItemURIs = store:read_items_of_type(TypeID),
-  Req:ok({"text/plain;charset=utf-8", utils:json(ItemURIs)});
+  send(Req, utils:json(ItemURIs));
 
 respond([<<"type">>, <<"_full">>], _, Req) ->
   TypeURIs = store:read_items_of_type(?TYPE),
   Types = [store_interface:read_type(TypeURI) || TypeURI <- TypeURIs],
-  Req:ok({"text/plain;charset=utf-8", utils:json(Types)});
+  send(Req, utils:json(Types));
 
 respond([TypeID, <<"_full">>], _, Req) ->
   ItemURIs = store:read_items_of_type(TypeID),
   Items = [store_interface:read_item(ItemURI, TypeID) || ItemURI <- ItemURIs],
-  Req:ok({"text/plain;charset=utf-8", utils:json(Items)});
+  send(Req, utils:json(Items));
 
 respond([<<"type">>, TypeID], _, Req) ->
   Type = store_interface:read_type(TypeID),
-  Req:ok({"text/plain;charset=utf-8", utils:json(Type)});
+  send(Req, utils:json(Type));
 
 respond([<<"property">>, URI], _, Req) ->
   Property = store_interface:read_property(URI),
-  Req:ok({"text/plain;charset=utf-8", utils:json(Property)});
+  send(Req, utils:json(Property));
 
 respond([TypeID, ItemID], _, Req) ->
   Item = store_interface:read_item(ItemID, TypeID),
-  io:format("~p~n", [Item]),
-  Req:ok({"text/plain;charset=utf-8", utils:json(Item)}).
+  send(Req, utils:json(Item)).
 
 log(Message) -> io:format("~p~n", [Message]).
+
+send(Req, Reply) -> Req:ok({"text/plain;charset=utf-8", Reply}).
