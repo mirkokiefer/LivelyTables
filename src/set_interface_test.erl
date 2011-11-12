ab15d69fa5fcc79411221630d5b031a0f3889a01
Mirko Kiefer <mail@mirkokiefer.com>
2011-11-12 17:16:51 +0200

    test set2records

--- src/set_interface_test.erl
+++ src/set_interface_test.erl
@@ -3,11 +3,17 @@
 
 run() ->
   store_sample(),
+  test_set2record(),
   {ok, success}.
 
 store_sample() ->
   Set = test_data:sample_set(),
   {atomic, {ok, success}} = t(fun() -> store_interface:write_item(Set) end).
+
+test_set2record() ->
+  SampleSet = test_data:sample_set(),
+  RecordSet = test_data:record_set(),
+  RecordSet == set_interface:set2records(SampleSet).
 
 t(Fun) ->
   store_interface:transaction(Fun).
