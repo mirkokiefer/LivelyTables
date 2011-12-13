f47e25c23e635ee85335113b889db8397532b343
Mirko Kiefer <mail@mirkokiefer.com>
2011-12-13 12:28:19 +0100

    ensure meta db to be existing on start

--- apps/livelytables/src/livelytables_app.erl
+++ apps/livelytables/src/livelytables_app.erl
@@ -10,6 +10,7 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    setup:ensure(),
     livelytables_sup:start_link().
 
 stop(_State) ->
